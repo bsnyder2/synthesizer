9117ffddef44dd9813672c0bc6ad822b1dd7a142
Ben Snyder <bsnyder2@users.noreply.github.com>
2023-07-14 12:25:00 -0400

    Simplified waveform equations, fixed stream_time issue

--- samplegenerator.cpp
+++ samplegenerator.cpp
@@ -13,20 +13,21 @@ void SampleGenerator::fillSamples(double *buffer, int n_frames, double stream_ti
 {
     for (int frame_i = 0; frame_i < n_frames; frame_i++)
     {
+        double adjustedAmplitude = amplitude / (double)255;
+        double adjustedFrequency = toHz() * (frame_i / (double)44100 + stream_time);
         switch (wave_shape)
         {
         case 0: // Sine wave
-            *buffer++ = (amplitude / (double)255) *
-                        sin(2 * M_PI * toHz() * ((frame_i / (double)44100) + stream_time));
+            *buffer++ = adjustedAmplitude *
+                        sin(2 * M_PI * adjustedFrequency);
             break;
         case 1: // Sawtooth wave
-            // FIX THIS
-            *buffer++ = (amplitude / (double)255) *
-                        2 * ((toHz() * frame_i / (double)44100) - floor(0.5 + (toHz() * frame_i / (double)44100)));
+            *buffer++ = adjustedAmplitude *
+                        2 * (adjustedFrequency - floor(0.5 + adjustedFrequency));
             break;
         case 2: // Square wave
-            *buffer++ = (amplitude / (double)255) *
-                        std::copysign(1.0, sin(2 * M_PI * toHz() * ((frame_i / (double)44100) + stream_time)));
+            *buffer++ = adjustedAmplitude *
+                        std::copysign(1.0, sin(2 * M_PI * adjustedFrequency));
             break;
         }
     }
